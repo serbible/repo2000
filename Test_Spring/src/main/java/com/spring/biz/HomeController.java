8added50360bcbaeebede07851cc0455bab59887
serbiblemaster <serbible@nate.com>
2019-06-04 16:07:30 +0900

    Update HomeController.java

--- Test_Spring/src/main/java/com/spring/biz/HomeController.java
+++ Test_Spring/src/main/java/com/spring/biz/HomeController.java
@@ -36,6 +36,7 @@ public class HomeController {
 		System.out.println("3번 추가");
 		System.out.println("7번 추가");
 		System.out.println("5번 추가");
+		System.out.println("55번 내려받기 추가");
 		return "home";
 	}
 	
